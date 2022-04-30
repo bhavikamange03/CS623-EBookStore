d7233e8b901e1084c8bb11845331e15535042937
Bhavika Mange <>
2022-04-29 17:24:36 -0700

    - added left filter code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import "./App.scss";
 import SearchBar from "./components/SearchBar/SearchBar";
 import BookData from "./data.json";
 import BooksGrid from "./components/BooksGrid/BooksGrid";
+import Filter from "./components/Filter/Filter";
 import { Row, Card, Col, Container } from 'react-bootstrap';
 
 function App() {
+
+  const filterConfig = {
+    filters: [
+      {
+        name: "categories",
+        id: "categories",
+        data: [
+          {id: "novel", label: "Novel", checked: false},
+          {id: "fiction", label: "Fiction", checked: false},
+          {id: "non-fiction", label: "Non Fiction", checked: false},
+          {id: "education", label: "Education", checked: false},
+          {id: "computer", label: "Computers", checked: false},
+          {id: "business", label: "Business", checked: false},
+          {id: "internet", label: "Internet", checked: false},
+        ]
+      },
+      {
+        name: "author",
+        id: "author",
+        data: [
+          {id: "W. Frank", label: "W. Frank", checked: false},
+          {id: "Charlie Collins", label: "Charlie Collins", checked: false},
+          {id: "Robi Sen", label: "Robi Sen", checked: false},
+          {id: "Faisal Abid", label: "Faisal Abid", checked: false},
+        ]
+      }
+    ]
+  }
+
+  const handleFilterChange = (config) => {
+    console.log('config', config);
+  };
+
+
   return (
     <div className="App">
       <Container fluid>
         <Row className="">
           <Col md={2}>
-            left filter
+            <Filter config={filterConfig} filterCallback={(config) => handleFilterChange(config)}></Filter>
           </Col>
           <Col md={10}>
             <SearchBar placeholder="Search" data={BookData}/>
@@ -19,8 +54,6 @@ function App() {
           </Col>
         </Row>
       </Container>
-      {/* <SearchBar placeholder="Enter a Book Name..." data={BookData} /> */}
-      {/* <BooksGrid data={BookData}/> */}
     </div>
   );
 }
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./Filter.scss";
+import { Row, Card, Col, Container } from 'react-bootstrap';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+function Filter({config, filterCallback}) {
+    const [selectedFilters, setFilters] = useState({});
+
+    const handleChange = ({event, subfilterId, filterId}) => {
+        const filters = JSON.parse(JSON.stringify(selectedFilters));
+
+        if (event?.target?.checked) {
+            filters[subfilterId] = event.target.checked;
+        } else {
+            if (filters[subfilterId]) {
+                delete filters[subfilterId];
+            }
+        }
+
+        setFilters(filters);
+        triggerCallback(filters);
+       
+    };
+
+    function triggerCallback(filters) {
+
+        filterCallback(filters);
+    }
+
+    return (
+        <Container fluid className="my-5">
+            <Row className="filter-container">
+                {config?.filters.map((filter, filterindex) => {
+                return (
+                    <div className="filter-item mb-4" key={`filter-${filterindex}`}>
+                        <div className="filter-type-label mb-2 text-uppercase">
+                            {filter?.name}
+                        </div>
+                        <div className="filter-sub-categories px-3">
+                            {filter?.data.map((subfilter, subfilterindex) => {
+                                return (
+                                    <div className="filter-sub-item py-1" key={`subfilter-${subfilterindex}`}>
+                                        <div className="d-inline-block align-center">
+                                            <Checkbox
+                                                className="filter-toggle-checkbox"
+                                                checked={selectedFilters[subfilter?.id] || false}
+                                                onChange={(event) => handleChange({event: event, subfilterId: subfilter.id, filterId: filter.id })}
+                                                inputProps={{ 'aria-label': 'controlled' }}
+                                            />
+                                        </div>
+                                        <div className="filter-sub-type-label d-inline-block align-center">
+                                            {subfilter?.label}
+                                        </div>
+                                    </div>
+                                )}
+                            )}
+                        </div>
+                    </div>
+                 )}
+                )}
+            </Row>
+        </Container>
+    );
+}
+
+
+export default Filter;
+
